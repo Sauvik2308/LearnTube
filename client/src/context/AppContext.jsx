88cb509ce39f29b418132faffe7e723ffe8b8457
Sauvik Das <sauvik.sgc2016@gmail.com>
2025-04-29 00:02:50 +0530

    Start building Course Details page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect } from "react";
 import { dummyCourses } from "../assets/assets";
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration';
 
 export const AppContext = createContext()
 
@@ -31,12 +32,41 @@ export const AppContextProvider = (props) => {
         return totalRating / course.courseRatings.length;
     }
 
+    // Function to calculate course chapter time
+        const calculateChapterTime = (chapter) => {
+            let time = 0;
+            chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+            return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})
+        }
+
+    // Function to calculate total course duration
+    const calculateCourseDuration = (course) => {
+        let time = 0
+
+        course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time += lecture.lectureDuration))
+
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})  
+    }
+
+    // Function to calculate number of lectures in the course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length
+            }
+        });
+
+        return totalLectures;
+    }
+
     useEffect(() => {
         fetchAllCourses()
     }, [])
     
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime
     }
 
     return (
