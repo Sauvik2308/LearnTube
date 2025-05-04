bfa89aa21bf320575232ef79598c3f56079742c1
Sauvik Das <sauvik.sgc2016@gmail.com>
2025-05-04 11:40:14 +0530

    Created the educator routes and controller

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import "dotenv/config";
 import { clerkWebhooks } from './controllers/webHooks.controller.js';
 import connectDB from './configs/mongodb.js';
+import educatorRouter from './routes/educator.routes.js';
+import { clerkMiddleware } from '@clerk/express';
 
 // Initialize Express
 const app = express()
@@ -12,12 +14,14 @@ await connectDB()
 
 // Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => {
     res.send("API Working")
 })
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
 
 // Port
 const PORT = process.env.PORT || 5000;
