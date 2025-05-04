467847a881b28e06682d9eab2fac9c8a3863a294
Sauvik Das <sauvik.sgc2016@gmail.com>
2025-05-04 10:53:59 +0530

    Fixed bugs

--- server/controllers/webHooks.controller.js
+++ server/controllers/webHooks.controller.js
@@ -7,7 +7,7 @@ export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(json.stringify(req.body), {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.created' : {
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
 
